7f1c54c4f472cb5b7943667516ee2fc7a6dab294
saadaouad <saad.aouad@bp.com>
2023-08-10 15:59:28 +0200

    Deploy

---
+++ src/db.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default prisma;
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+import config from './config';
+import app from './server';
+
+dotenv.config();
+
+app.listen(config.port, () => {
+    console.log(`hello on http://localhost:${config.port}/`);
+});
